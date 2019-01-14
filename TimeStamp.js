bbd5984984bcf2fd86b253ef143a029249215351
Peter-S <32533746+Peter-S@users.noreply.github.com>
2019-01-13 17:38:56 -0800

    Added alert and console
    Added an alert popup with the date and also log the date to the console.

--- TimeStamp.js
+++ TimeStamp.js
@@ -1,4 +1,7 @@
 function timeStamp() {
     'use strict';
-    document.getElementById('demo').innerHTML = Date();
+    //document.getElementById('demo').innerHTML = Date();
+	var d = new Date;
+	alert(d);
+	console.log(d);
 }
